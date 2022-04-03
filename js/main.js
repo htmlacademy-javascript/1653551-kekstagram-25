dae16c839f283eb41b7d2459d6f56cb69af33039
Николь <n.kozerenko@yandex.ru>
2022-04-03 16:13:29 +0300

    Выполняет кривую корявую черновую версию задания

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-import {makePost} from './generate-posts';
+import { makePost } from './generate-posts';
+import { drawMiniatures } from './draw-miniatures';
 
 //Основная логика
 //Запуск основной логики
-const posts = Array.from({ length: 25 }, makePost);
-
-// Временные запуски утилит на период разработки
-// @todo: не забыть удалить
-console.log(posts);
+drawMiniatures(makePost());
