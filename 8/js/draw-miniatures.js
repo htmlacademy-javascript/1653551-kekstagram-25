78f97b9d52f2d21acf2272f867825dd7fe3d5053
Keks <keks@htmlacademy.ru>
2022-04-09 03:54:35 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/draw-miniatures.js
@@ -0,0 +1,24 @@
+// @ts-nocheck
+import { renderBigPicture } from './gallery.js';
+
+const thumbnailsContainer = document.querySelector('.pictures');
+const thumbnailTemplateElement = document.querySelector('#picture');
+const thumbnailTemplate = thumbnailTemplateElement.content.querySelector('.picture'); //копируем ссылку с классом picture из шаблона
+
+const renderThumbnails = function (posts) {
+  const thumbnailsFragment = document.createDocumentFragment();
+  posts.forEach((post) => {
+    const thumbnailElement = thumbnailTemplate.cloneNode(true);
+    thumbnailElement.querySelector('.picture__img').src = post.url;
+    thumbnailElement.querySelector('.picture__likes').textContent = post.likes;
+    thumbnailElement.querySelector('.picture__comments').textContent = post.comments.length;
+    thumbnailsFragment.appendChild(thumbnailElement);
+
+    thumbnailElement.addEventListener('click', () => {
+      renderBigPicture(post);
+    });
+  });
+  thumbnailsContainer.appendChild(thumbnailsFragment);
+};
+
+export { renderThumbnails };
