1cbeb12c0a2c3b60aa6f7ac0da4e29d0e6d2102a
Николь <n.kozerenko@yandex.ru>
2022-04-12 06:42:51 +0300

    Выполняет загрузку пользовательских данных и отправку формы

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-import { makePosts } from './generate-posts.js';
+//import { makePosts } from './generate-posts.js';
 import { renderThumbnails } from './draw-miniatures.js';
-import { POSTS_COUNT } from './constants.js';
+//import { POSTS_COUNT } from './constants.js';
 import './handle-forms.js';
 import './gallery.js';
-import  './filters.js';
+import './filters.js';
+import { getDataFromServer } from './server-api.js';
 
 //Основная логика
 //Запуск основной логики
-renderThumbnails(makePosts(POSTS_COUNT));
+//renderThumbnails(makePosts(POSTS_COUNT));
+const onGetDataError = () => {};
+
+const onGetDataSuccess = (posts) => {
+  renderThumbnails(posts);
+};
+
+getDataFromServer(onGetDataSuccess, onGetDataError);
+
