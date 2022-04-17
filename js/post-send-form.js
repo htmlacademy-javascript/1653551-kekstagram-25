89c4ec95505cc154b9926ab4243f1df1a349b054
Николь <n.kozerenko@yandex.ru>
2022-04-17 19:43:31 +0300

    Вносит ещё несколько правок по защите

--- js/post-send-form.js
+++ js/post-send-form.js
@@ -1,8 +1,5 @@
 import { isEscapeKey } from './utils/common.js';
-import {
-  enableFilters,
-  disableFilters,
-} from './upload-image-filters.js';
+import { enableFilters, disableFilters } from './upload-image-filters.js';
 import { sendPost } from './api/services.js';
 import { openUploadResultModal, UploudMessageModalType } from './upload-modals.js';
 import { resetPreviewScaleControlls } from './post-preview.js';
@@ -10,38 +7,42 @@ import formValidator from './post-send-form-validate.js';
 
 const bodyElement = document.body;
 const uploadInputElement = document.querySelector('#upload-file');
-const modalContainer = document.querySelector('.img-upload__overlay');
-const uploadForm = document.querySelector('.img-upload__form');
-const submitButton = uploadForm.querySelector('#upload-submit');
-const cancelButton = document.querySelector('#upload-cancel');
-const hashtagInput = document.querySelector('.text__hashtags');
+const modalContainerElement = document.querySelector('.img-upload__overlay');
+const uploadFormElement = document.querySelector('.img-upload__form');
+const submitButtonElement = uploadFormElement.querySelector('#upload-submit');
+const cancelButtonElement = document.querySelector('#upload-cancel');
+const hashtagInputElement = document.querySelector('.text__hashtags');
 const previewImgElement = document.querySelector('.img-upload__preview img');
-const commentInput = document.querySelector('.text__description');
+const commentInputElement = document.querySelector('.text__description');
 
 // Признак текущей отправки формы создания нового поста
 let isUploadFormSending = false;
 
-const onShowPostUploadForm = function () {
+function onShowPostUploadForm() {
   const file = this.files[0];
-  modalContainer.classList.remove('hidden');
+
+  modalContainerElement.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
+
   document.addEventListener('keydown', onEscPress);
+
   enableFilters();
   resetPreviewScaleControlls();
+
   const fileReader = new FileReader();
   fileReader.onload = (evt) => {
     previewImgElement.src = evt.target.result;
   };
   fileReader.readAsDataURL(file);
-};
+}
 
-const closeModal = function () {
-  modalContainer.classList.add('hidden');
+const closeModal = () => {
+  modalContainerElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onEscPress);
   disableFilters();
   resetPreviewScaleControlls();
-  uploadForm.reset();
+  uploadFormElement.reset();
 };
 
 function onEscPress(evt) {
@@ -51,19 +52,19 @@ function onEscPress(evt) {
 }
 
 const blockSubmitButton = () => {
-  submitButton.disabled = true;
-  submitButton.textContent = 'Публикуем...';
+  submitButtonElement.disabled = true;
+  submitButtonElement.textContent = 'Публикуем...';
 };
 
 const unblockSubmitButton = () => {
-  submitButton.disabled = false;
-  submitButton.textContent = 'Опубликовать';
+  submitButtonElement.disabled = false;
+  submitButtonElement.textContent = 'Опубликовать';
 };
 
-const onSendPostSuccess = function () {
+const onSendPostSuccess = () => {
   openUploadResultModal(UploudMessageModalType.SUCCESS);
 };
-const onSendPostFailed = function () {
+const onSendPostFailed = () => {
   openUploadResultModal(UploudMessageModalType.ERROR);
 };
 
@@ -75,29 +76,32 @@ const onSendPostFinnaly = () => {
 
 uploadInputElement.addEventListener('change', onShowPostUploadForm);
 
-hashtagInput.addEventListener('keydown', (evt) => {
+hashtagInputElement.addEventListener('keydown', (evt) => {
   if (isEscapeKey(evt)) {
     evt.stopPropagation();
   }
 });
 
-commentInput.addEventListener('keydown', (evt) => {
+commentInputElement.addEventListener('keydown', (evt) => {
   if (isEscapeKey(evt)) {
     evt.stopPropagation();
   }
 });
 
 // Слушатель отправки формы загрузки нового поста
-uploadForm.addEventListener('submit', (evt) => {
+uploadFormElement.addEventListener('submit', (evt) => {
   if (isUploadFormSending) {
     return;
   }
+
   evt.preventDefault();
+
   const isFormValid = formValidator.validate();
   if (!isFormValid) {
     return;
   }
-  const formData = new FormData(uploadForm);
+
+  const formData = new FormData(uploadFormElement);
 
   blockSubmitButton();
   isUploadFormSending = true;
@@ -105,6 +109,6 @@ uploadForm.addEventListener('submit', (evt) => {
 });
 
 
-cancelButton.addEventListener('click', () => {
+cancelButtonElement.addEventListener('click', () => {
   closeModal();
 });
